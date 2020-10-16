164f14e2699c267735c4963bf1ecba51de38181f
Shujaat Azim <shujaat.azim@gmail.com>
2020-10-15 23:59:31 -0400

    Built out the controller for the join table, just in case we need to access it from the front end.

--- app/controllers/day_activities_controller.rb
+++ app/controllers/day_activities_controller.rb
@@ -1,2 +1,13 @@
 class DayActivitiesController < ApplicationController
+
+  def index
+    @day_activities = DayActivity.all
+    render json: @day_activities.to_json()
+  end
+
+  def show
+    @day_activity = DayActivity.find(params[:id])
+    render json: @day_activity.to_json()
+  end
+  
 end
